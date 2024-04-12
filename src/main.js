2804eab82d0d37c0d8010e6a2c24f5df43d51053
mohamed-fm <141369494+mohamed-fm@users.noreply.github.com>
2024-04-12 07:06:32 +0200

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,34 @@
+
+
+
+//category filter
+let switcher = document.querySelectorAll(".ull li");
+let posts = Array.from(document.querySelectorAll(".posts .post"));
+let x = document.querySelector(".post");
+console.log(x);
+switcher.forEach((li)=>{
+    li.addEventListener("click",removeactive);
+    li.addEventListener("click",imgmanage);
+
+});
+
+function  removeactive() {
+    switcher.forEach((li) =>{
+        li.classList.remove("active");
+        this.classList.add("active");
+        
+    });
+}
+
+
+
+function imgmanage() {
+    posts.forEach((x) =>{
+        x.style.display="none";
+    });
+    console.log(document.querySelectorAll(this.dataset.cat));
+    document.querySelectorAll(this.dataset.cat).forEach((el) => {
+        el.style.display="block";
+    });
+}
+
