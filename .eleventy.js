6e9c77ebd060721da1b45ef2bfa0178b409d0ed7
mohamed-fm <141369494+mohamed-fm@users.noreply.github.com>
2024-04-12 07:07:20 +0200

    Add files via upload

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+
+
+module.exports = function(eleventyconfig){
+    const {DateTime} = require("luxon")
+    eleventyconfig.addPassthroughCopy('./src/style.css');
+    eleventyconfig.addPassthroughCopy('./src/main.js');
+    eleventyconfig.addPassthroughCopy('./src/assets');
+    eleventyconfig.addFilter("postDate", dateObj => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+    });
+    
+    return {
+        dir: {
+            input:"src",
+            output:"public"
+        }
+    };
+}
