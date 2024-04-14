b6f8403a06ddf137bb7e4424a02540a78fe4d539
mohamed-fm <141369494+mohamed-fm@users.noreply.github.com>
2024-04-14 09:48:46 +0200

    Add files via upload

--- public/main.js
+++ public/main.js
@@ -1,5 +1,20 @@
 
 
+const activePage = window.location.pathname;
+console.group(activePage);
+if(activePage === "/index"){
+    document.querySelector(".home").classList.add("activee");
+}
+if(activePage === "/Our-mission/"){
+    document.querySelector(".our").classList.add("activee");
+}
+if(activePage === "/artical/"){
+    document.querySelector(".art").classList.add("activee");
+}
+if(activePage === "/join-us/"){
+    document.querySelector(".join").classList.add("activee");
+}
+
 
 //category filter
 let switcher = document.querySelectorAll(".ull li");
@@ -28,7 +43,7 @@ function imgmanage() {
     });
     console.log(document.querySelectorAll(this.dataset.cat));
     document.querySelectorAll(this.dataset.cat).forEach((el) => {
-        el.style.display="block";
+        el.style.display="inherit";
     });
 }
 
