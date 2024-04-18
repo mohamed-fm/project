b72e53d93e6f3b68380c650765c06d4f95a907ca
mohamed-fm <141369494+mohamed-fm@users.noreply.github.com>
2024-04-18 12:26:01 +0200

    Add files via upload

--- public/main.js
+++ public/main.js
@@ -1,7 +1,4 @@
-
-
 const activePage = window.location.pathname;
-console.group(activePage);
 if(activePage === "/home/"){
     document.querySelector(".home").classList.add("activee");
 }
@@ -14,19 +11,24 @@ if(activePage === "/artical/"){
 if(activePage === "/join-us/"){
     document.querySelector(".join").classList.add("activee");
 }
-
-
 //category filter
+
+
 let switcher = document.querySelectorAll(".ull li");
 let posts = Array.from(document.querySelectorAll(".posts .post"));
 let x = document.querySelector(".post");
-console.log(x);
-switcher.forEach((li)=>{
-    li.addEventListener("click",removeactive);
-    li.addEventListener("click",imgmanage);
 
-});
 
+    switcher.forEach((li)=>{
+        li.addEventListener("click",CAT);
+    });
+
+function CAT(){
+    switcher.forEach((li)=>{
+        li.addEventListener("click",removeactive);
+        li.addEventListener("click",imgmanage);
+    });
+}
 function  removeactive() {
     switcher.forEach((li) =>{
         li.classList.remove("active");
@@ -41,9 +43,48 @@ function imgmanage() {
     posts.forEach((x) =>{
         x.style.display="none";
     });
-    console.log(document.querySelectorAll(this.dataset.cat));
     document.querySelectorAll(this.dataset.cat).forEach((el) => {
         el.style.display="flex";
     });
 }
 
+var swiper = new Swiper('.swiper',{
+    // Optional parameters
+    slidesPerView:2,
+    spaceBetween: 30,
+  
+    // If we need pagination
+   
+    pagination: {
+        el: '.swiper-pagination',
+      },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+      }
+  });
+
+
+window.addEventListener("resize", function(event) {
+    console.log(document.body.clientWidth)
+    if (document.body.clientWidth < 800) {
+        const swiper = new Swiper('.swiper',{
+            // Optional parameters
+            // Navigation arrows
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            // And if we need scrollbar
+            scrollbar: {
+                el: '.swiper-scrollbar',
+            },
+        });   
+        document.querySelector(".fe-small").style.margin = '0'     
+    }
+});
